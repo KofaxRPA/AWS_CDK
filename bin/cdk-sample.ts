a08e4dfe76a6328bfee5a38ae0e52d4544cc5a6f
David Wright <david.wright@kofax.com>
2022-01-19 16:41:28 +0100

    stackname now includes da of month

--- bin/cdk-sample.ts
+++ bin/cdk-sample.ts
@@ -5,7 +5,7 @@ import { KofaxRPAStack } from '../lib/KofaxRPA-stack';
 const app = new cdk.App();
 var today = new Date();
 // var StackName ='RPA-'+dateAsYYYYMMDDHHNNSS(today);
-var StackName ='RPA-'+leftpad(today.getHours(),2) + leftpad(today.getMinutes(),2);
+var StackName ='RPA-'+ leftpad(today.getDate(), 2)+'-'+ leftpad(today.getHours(),2) + leftpad(today.getMinutes(),2);
 new KofaxRPAStack(app, StackName);
 
 function dateAsYYYYMMDDHHNNSS(date: Date): string {
